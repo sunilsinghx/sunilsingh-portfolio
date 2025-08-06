1b5c2b75e53e5677fead7ff70268a7a1bc1aaebd
Sunil Singh <sunilsingh09012@gmail.com>
2025-08-06 23:08:56 +0530

    updated project section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,6 +14,68 @@ const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Chatapp with Ollama </h3>
+              <p className="text-gray-400 mb-4">
+              ChatGPT-style App using LLaMA 2 & Ollama
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Next.js","Node.js","PostgreSQL","LLaMA 2"].map((tech, key) => (
+                  <span
+                  key={key}
+                  className="
+                    bg-blue-500/10 text-blue-500 py-1 px-3 
+                    rounded-full text-sm
+                    transition
+                    hover:bg-blue-500/20 hover:-translate-y-0.5
+                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                  "
+                >
+                  {tech}
+                </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/sunilsinghx/chatapp-with-Ollama"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Disaster Response Coordination </h3>
+              <p className="text-gray-400 mb-4">
+              A backend-heavy disaster management built with Node.js and React.js, designed to assist emergency response using geospatial data, AI, and social media post integrations.
+
+
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React.js","Node.js","Supabase","Google Gemini","PostGIS","Bluesky API"].map((tech, key) => (
+                  <span
+                  key={key}
+                  className="
+                    bg-blue-500/10 text-blue-500 py-1 px-3 
+                    rounded-full text-sm
+                    transition
+                    hover:bg-blue-500/20 hover:-translate-y-0.5
+                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                  "
+                >
+                  {tech}
+                </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/sunilsinghx/disaster-response-coordination"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2">BlogApp</h3>
               <p className="text-gray-400 mb-4">
               Full-stack MERN blog app with seamless CRUD operations for blog posts.Used Docker Compose for orchestration
@@ -43,6 +105,7 @@ const Projects = () => {
                 </a>
               </div>
             </div>
+            
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
